c9113b52fc10df1bf9bd116bc381c5dc375eaac3
CountrySideEngineer <deep-sleeper@nifty.com>
2023-09-25 21:42:09 +0900

    Start developing, implementing the application.

---
+++ dev/DBConnector/DBConnector/DTO/DTOBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBConnector.DTO
+{
+    public class DTOBase
+    {
+        /// <summary>
+        /// Record id, primary key.
+        /// </summary>
+        public int ID { get; set; } = 0;
+
+        /// <summary>
+        /// Date time the record created at.
+        /// </summary>
+        public DateTime CreatedAt { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Date time the record updated at last time.
+        /// </summary>
+        public DateTime UpdatedAt { get; set; } = DateTime.MinValue;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public DTOBase() { }
+    }
+}
---
+++ dev/DBConnector/DBConnector/IDbConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBConnector.IF
+{
+    internal interface IDbConnector<Reader> : IDisposable
+    {
+        Reader ExecuteQuery(string query);
+        Reader ExecuteQuery(string query, Dictionary<string, object> parameters);
+
+        int ExecuteNonQuery(string query);
+        int ExecuteNonQuery(string query, Dictionary<string, object> parameters);
+
+        void Connect();
+        void DisConnect();
+
+        string SetupConnectionString();
+
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+    }
+}
