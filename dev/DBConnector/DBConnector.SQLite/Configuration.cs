6de58fecc695b1f8605822b62f9f6f6ec6cb8bde
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-01 17:25:07 +0900

    Add project to dll to be used to connect SQLite data base.

---
+++ dev/DBConnector/DBConnector.SQLite/Configuration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DBConnector.SQLite
+{
+    [XmlRoot("Configuration")]
+    internal class Configuration
+    {
+        protected static string _configPath = @".\SQLiteConfiguration.xml";
+        public static Configuration Load()
+        {
+            using (var stream = new FileStream(_configPath, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Configuration));
+                Configuration config = (Configuration)serializer.Deserialize(stream);
+                return config;
+            }
+        }
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        protected Configuration()
+        {
+            Path = string.Empty;
+        }
+
+        [XmlElement("Path")]
+        public string Path { get; set; }
+
+
+    }
+}
