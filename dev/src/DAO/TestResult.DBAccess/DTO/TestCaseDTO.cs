8ad8150feddb765383f4b11c35b9a29929d8431f
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-09 06:48:07 +0900

    Add and update about test cases. Add TestCaseDAO, DAO class to access "test_cases" table. Update TestCaseDTO class by adding and changing properties corresponding to columns in the table.

--- dev/src/DAO/TestResult.DBAccess/DTO/TestCaseDTO.cs
+++ dev/src/DAO/TestResult.DBAccess/DTO/TestCaseDTO.cs
@@ -9,13 +9,15 @@ namespace TestResult.DBAccess.DTO
 {
 	public class TestCaseDTO : DTOBase
 	{
-		public string TestCode { get; set; } = string.Empty;
+		public string TestName { get; set; } = string.Empty;
 
 		public string Summary {  get; set; } = string.Empty;
 
 		public string Detail { get; set; } = string.Empty;
 
 		public TestCaseDTO? BaseTestCase { get; set; } = null;
+
+		public int TestCaseVersion { get; set;} = 1;
 
 		/// <summary>
 		/// Default constructor.
