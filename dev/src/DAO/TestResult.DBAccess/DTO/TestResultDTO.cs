37de3100d29ae4067d907d185aa5405cf11b9d67
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-10 22:28:56 +0900

    Add and start tp develop TestResultDTO class.

---
+++ dev/src/DAO/TestResult.DBAccess/DTO/TestResultDTO.cs
@@ -0,0 +1,15 @@
+﻿using DBConnector.DTO;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestResult.DBAccess.DTO
+{
+	public class TestResultDTO : DTOBase
+	{
+	}
+}
