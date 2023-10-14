4607ec77a17a66ca296805e555fa07a6ad450069
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-14 19:45:33 +0900

    Add and update to create table in data base.

--- dev/db/CREATE_TABLEs.sql
+++ dev/db/CREATE_TABLEs.sql
@@ -62,6 +62,19 @@ CREATE TABLE "testers" (
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
 
+CREATE TABLE "test_versions" (
+	"id"	INTEGER,
+	"version_code"	TEXT NOT NULL,
+	"previous_version_code_id"	INTEGER DEFAULT 0,
+	"products_id"	INTEGER NOT NULL DEFAULT 0,
+	"created_at"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	"updated_at"	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY("id"),
+	FOREIGN KEY("products_id") REFERENCES "products"("id") ON UPDATE CASCADE,
+	FOREIGN KEY("previous_version_code_id") REFERENCES "test_versions"("id") ON UPDATE CASCADE ON DELETE SET NULL,
+	UNIQUE("version_code","products_id")
+);
+
 CREATE TABLE "test_results" (
 	"id"	INTEGER,
 	"products_id"	INTEGER,
