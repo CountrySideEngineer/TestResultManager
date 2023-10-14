490792699e51c9df13ccba69441eefe27398f5ce
CountrySideEngineer <deep-sleeper@nifty.com>
2023-10-14 14:52:15 +0900

    Update SQL query text file to create test_result_codes table.

--- dev/db/CREATE_test_result_codes_TABLE.sql
+++ dev/db/CREATE_test_result_codes_TABLE.sql
@@ -1,7 +1,9 @@
 CREATE TABLE "test_result_codes" (
 	"id"	INTEGER,
-	"result_text"	TEXT NOT NULL UNIQUE,
+	"result_text"	TEXT NOT NULL,
+	"output_text"	TEXT NOT NULL,
 	"created_at"	TEXT DEFAULT CURRENT_TIMESTAMP,
 	"updated_at"	TEXT DEFAULT CURRENT_TIMESTAMP,
+	UNIQUE("result_text","output_text"),
 	PRIMARY KEY("id" AUTOINCREMENT)
 );
